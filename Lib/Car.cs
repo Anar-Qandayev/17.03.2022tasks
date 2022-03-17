f1f261c6c0a7e48f77d673b5837b33870b5c0fdc
Anar-Qandayev <tu7bxfga8@code.edu.az>
2022-03-17 13:52:04 +0400

    commit

---
+++ Lib/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lib
+{
+    class Car:Edit
+    {        
+        public void Start()
+        {
+            Model = "jzdgv";
+            //Edit ed = new Edit();
+
+
+        }
+    }
+}
